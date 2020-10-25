67b6a288b2a10ce455dd0e92a307f8723d0bc599
Nobody <73292315+Nobody5-creator@users.noreply.github.com>
2020-10-25 17:05:29 +0100

    Sign Up
    L'interface-Inscription

---
+++ Doc2.js
@@ -0,0 +1,4 @@
+document.getElementById("Login2").addEventListener("submit", function(e){
+    e.preventDefault();
+    alert("You signed up Successfully, you can now sign in to your account!");
+});
