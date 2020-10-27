247d97fac3294e7faa4739061e69c0f2c537d87a
fendorei <antoine.luta@gmail.com>
2020-10-27 01:07:44 +0100

    attempt

--- Doc1.js
+++ Doc1.js
@@ -1,7 +1,4 @@
-
 document.getElementById("Login1").addEventListener("submit", function(e){
     e.preventDefault();
     alert("Welcome to your account, You signed in Successfully!");
 });
-
-
--- Doc2.js
+++ Doc2.js
@@ -2,3 +2,10 @@ document.getElementById("Login2").addEventListener("submit", function(e){
     e.preventDefault();
     alert("You signed up Successfully, you can now sign in to your account!");
 });
+
+let username = document.getElementById("pseudo")
+localStorage.setItem("username", username.value)
+let email = document.getElementById("email")
+localStorage.setItem("email", email.value)
+let password = document.getElementById("mdp")
+localStorage.setItem("password", password.value)
